c31a3ce74b604bed6ef54ef67a6195d991c600a7
Soumith Chintala <soumith@fb.com>
2014-10-27 22:23:39 -0400

    adding codemirror lua highlighting

---
+++ custom.js
@@ -0,0 +1,27 @@
+$([IPython.events]).on('notebook_loaded.Notebook', function(){
+    // add here logic that should be run once per **notebook load**
+    // (!= page load), like restarting a checkpoint
+    var md = IPython.notebook.metadata
+    if(md.language){
+        console.log('language already defined and is :', md.language);
+    } else {
+        md.language = 'lua' ;
+	console.log('add metadata hint that language is lua');
+    }
+});
+$([IPython.events]).on("app_initialized.NotebookApp", function () {
+    console.log('here');
+    IPython.CodeCell.options_default['cm_config']['mode'] = 'lua';
+
+    CodeMirror.requireMode('lua', function(){
+        cells = IPython.notebook.get_cells();
+        for(var i in cells){
+            c = cells[i];
+            if (c.cell_type === 'code'){
+                c.auto_highlight()
+            }
+        }
+    });
+    
+
+});
