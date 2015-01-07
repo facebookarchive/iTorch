fe8ecb1ad6dded88d1fbef35c7d7ae286ed921ce
Soumith Chintala <soumith@fb.com>
2015-01-07 14:58:30 -0500

    test/example for audio and video

--- test.lua
+++ test.lua
@@ -1,12 +1,18 @@
 -- images
--- itorch.lena()
--- itorch.image({image.lena(), image.lena(), image.lena()})
+itorch.image(image.lena())
+itorch.image({image.lena(), image.lena(), image.lena()})
+
+-- audio
+itorch.audio('volkswagen.mp3')
+
+-- video
+itorch.video('chase.ogv')
 
 -- html
--- itorch.html('<p><b>Hi there!</b> how are you</p>')
--- window_id = itorch.html('<p>This text will be replaced in 2 seconds</p>')
--- os.execute('sleep 2')
--- itorch.html('<p>magic!</p>', window_id)
+itorch.html('<p><b>Hi there!</b> how are you</p>')
+window_id = itorch.html('<p>This text will be replaced in 2 seconds</p>')
+os.execute('sleep 2')
+itorch.html('<p>magic!</p>', window_id)
 
 x1 = torch.randn(40):mul(100)
 y1 = torch.randn(40):mul(100)
@@ -22,6 +28,9 @@ plot:add(x3,y3,'green', 'yolo'):redraw()
 plot:title(' My plot!'):redraw()
 plot:xaxis('length'):yaxis('width'):redraw()
 plot:legend(true)
--- plot:redraw()
+plot:redraw()
 -- print(plot:toHTML())
 plot:save('out.html')
+
+
+
