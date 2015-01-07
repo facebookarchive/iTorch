120b360f5ecb0cc64000306decb35006ba269e0a
Soumith Chintala <soumith@fb.com>
2015-01-07 14:54:09 -0500

    adding itorch.audio and itorch.video

--- gfx.lua
+++ gfx.lua
@@ -54,6 +54,106 @@ function itorch.image(img)
    end
 end
 
+local audio_template = [[
+<div id="${div_id}"><audio controls src="data:audio/${extension};base64,${base64audio}" /></div>   
+]]
+-- Example: itorch.audio('hello.mp3')
+function itorch.audio(fname)
+   assert(itorch._iopub,'itorch._iopub socket not set')
+   assert(itorch._msg,'itorch._msg not set')
+   
+   -- get prefix
+   local pos = fname:reverse():find('%.')
+   local ext = fname:sub(#fname-pos + 2)
+   assert(ext == 'mp3' or ext == 'wav' or ext == 'ogg' or ext == 'aac', 
+	  'mp3, wav, ogg, aac files supported. But found extension: ' .. ext)
+   -- load the audio as binary blob
+   local f = assert(torch.DiskFile(fname,'r',true), 
+		    'File could not be opened: ' .. fname):binary();
+   f:seekEnd();
+   local size = f:position()-1
+   f:seek(1)
+   local buf = torch.CharStorage(size);
+   assert(f:readChar(buf) == size, 'wrong number of bytes read')
+   f:close()
+   local base64audio = base64.encode(ffi.string(torch.data(buf), size))
+   local div_id = uuid.new()
+   local content = {}
+   content.source = 'itorch'
+   content.data = {}
+   content.data['text/html'] =
+      audio_template % {
+         div_id = div_id,
+         extension = ext,
+	 base64audio = base64audio
+      };
+   content.metadata = {}
+   local header = tablex.deepcopy(itorch._msg.header)
+   header.msg_id = uuid.new()
+   header.msg_type = 'display_data'
+
+   -- send displayData
+   local m = {
+      uuid = itorch._msg.uuid,
+      content = content,
+      parent_header = itorch._msg.header,
+      header = header
+   }
+   util.ipyEncodeAndSend(itorch._iopub, m)
+   return window_id
+end
+
+local video_template = [[
+<div id="${div_id}"><video controls src="data:video/${extension};base64,${base64video}" /></div>   
+]]
+-- Example: itorch.video('hello.mp4')
+function itorch.video(fname)
+   assert(itorch._iopub,'itorch._iopub socket not set')
+   assert(itorch._msg,'itorch._msg not set')
+   
+   -- get prefix
+   local pos = fname:reverse():find('%.')
+   local ext = fname:sub(#fname-pos + 2)
+   if ext == 'ogv' then ext = 'ogg' end
+   assert(ext == 'mp4' or ext == 'wav' or ext == 'ogg' or ext == 'webm',
+	  'mp4, ogg, webm files supported. But found extension: ' .. ext)
+
+   -- load the video as binary blob
+   local f = assert(torch.DiskFile(fname,'r',true), 
+		    'File could not be opened: ' .. fname):binary();
+   f:seekEnd();
+   local size = f:position()-1
+   f:seek(1)
+   local buf = torch.CharStorage(size);
+   assert(f:readChar(buf) == size, 'wrong number of bytes read')
+   f:close()
+   local base64video = base64.encode(ffi.string(torch.data(buf), size))
+   local div_id = uuid.new()
+   local content = {}
+   content.source = 'itorch'
+   content.data = {}
+   content.data['text/html'] =
+      video_template % {
+         div_id = div_id,
+         extension = ext,
+	 base64video = base64video
+      };
+   content.metadata = {}
+   local header = tablex.deepcopy(itorch._msg.header)
+   header.msg_id = uuid.new()
+   header.msg_type = 'display_data'
+
+   -- send displayData
+   local m = {
+      uuid = itorch._msg.uuid,
+      content = content,
+      parent_header = itorch._msg.header,
+      header = header
+   }
+   util.ipyEncodeAndSend(itorch._iopub, m)
+   return window_id
+end
+
 function itorch.lena()
    itorch.image(image.lena())
 end
