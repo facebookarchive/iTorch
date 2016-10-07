488ac0149b67035c1bf362d04929ea0c21fe61fa
Nick Hynes <nhynes@mit.edu>
2016-10-07 07:30:53 -0700

    Allow redef of torch classes
    Summary:
    Running code that defines classes using `torch.class` in an interactive environment leads to the well-intentioned but undesirable `parent class/factor is already defined` error.
    
    This PR introduces a hacky workaround that silently unsets the existing class if it already exists.
    Closes https://github.com/facebook/iTorch/pull/136
    
    Reviewed By: soumith
    
    Differential Revision: D3982016
    
    Pulled By: ebetica
    
    fbshipit-source-id: 4fa402ee9254e9127025910ea0ef9a4ea6535a55

--- init.lua
+++ init.lua
@@ -11,5 +11,20 @@ itorch=require 'itorch._env'
 require 'itorch.gfx'
 require 'itorch.bokeh'
 itorch.Plot=require 'itorch.Plot'
+
+local _class = torch.class
+torch.class = function(name, parentName, module)
+  if name ~= nil then
+    debug.getregistry()[name] = nil
+  end
+  if module ~= nil then
+    return _class(name, parentName, module)
+  elseif parentName ~= nil then
+    return _class(name, parentName)
+  else
+    return _class(name)
+  end
+end
+
 return itorch
 
